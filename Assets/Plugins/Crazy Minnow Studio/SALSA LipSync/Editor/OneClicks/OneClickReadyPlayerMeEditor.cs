e3e91c2b86e72bc09152d2e0497e96d35a70f3ec
bernuly <bernuly@gmail.com>
2023-08-04 13:06:19 +0100

    Adding SALSA lipsync, OneClickBase, and OneClickRPM

---
+++ Assets/Plugins/Crazy Minnow Studio/SALSA LipSync/Editor/OneClicks/OneClickReadyPlayerMeEditor.cs
@@ -0,0 +1,87 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace CrazyMinnow.SALSA.OneClicks
+{
+	/// <summary>
+	/// RELEASE NOTES:
+	///		2.6.0-BETA:
+	///			+ NOTE: this OneClick requires OneClickBase v2.6.0.0+.
+	///			~ Adjusted default blendshape values to 0 - 1 scale vs Unity
+	///				standard 0 - 100. ReadyPlayerMe responded to inquiry
+	///				indicating they are sticking with this 0 - 1 weight scale.
+	///				Utility to convert back and forth will not be included in
+	///				this package.
+	///		2.5.1-BETA:
+	///			+ Support for atlas-generated-models with single mesh.
+	///			+ Utility to adjust blendshape weight configurations
+	///				from 0 - 100 to 0 - 1. Current version of RPM GLTF import
+	///				uses 0 - 1 instead of Unity standard 0 - 100.
+	///		2.5.0-BETA : Initial release.
+	/// ==========================================================================
+	/// PURPOSE: This script provides simple, simulated lip-sync input to the
+	///		Salsa component from text/string values. For the latest information
+	///		visit crazyminnowstudio.com.
+	/// ==========================================================================
+	/// DISCLAIMER: While every attempt has been made to ensure the safe content
+	///		and operation of these files, they are provided as-is, without
+	///		warranty or guarantee of any kind. By downloading and using these
+	///		files you are accepting any and all risks associated and release
+	///		Crazy Minnow Studio, LLC of any and all liability.
+	/// ==========================================================================
+	/// </summary>
+	public class OneClickReadyPlayerMeEditor : Editor
+	{
+		public delegate void SalsaOneClickChoice(GameObject gameObject, AudioClip audioClip);
+		public static SalsaOneClickChoice SalsaOneClickSetup = OneClickReadyPlayerMe.Setup;
+
+		public delegate void EyesOneClickChoice(GameObject gameObject);
+		public static EyesOneClickChoice EyesOneClickSetup = OneClickReadyPlayerMeEyes.Setup;
+
+		[MenuItem("GameObject/Crazy Minnow Studio/SALSA LipSync/One-Clicks/ReadyPlayerMe")]
+		public static void OneClickSetup_RPM()
+		{
+			SalsaOneClickSetup = OneClickReadyPlayerMe.Setup;
+			EyesOneClickSetup = OneClickReadyPlayerMeEyes.Setup;
+
+			OneClickSetup();
+		}
+
+		public static void OneClickSetup()
+		{
+			GameObject go = Selection.activeGameObject;
+			if (go == null)
+			{
+				Debug.LogWarning(
+					"NO OBJECT SELECTED: You must select an object in the scene to apply the OneClick to.");
+				return;
+			}
+
+#if UNITY_2018_3_OR_NEWER
+				if (PrefabUtility.IsPartOfAnyPrefab(go))
+				{
+					if (EditorUtility.DisplayDialog(
+													OneClickBase.PREFAB_ALERT_TITLE,
+													OneClickBase.PREFAB_ALERT_MSG,
+													"YES", "NO"))
+					{
+						PrefabUtility.UnpackPrefabInstance(go, PrefabUnpackMode.OutermostRoot, InteractionMode.AutomatedAction);
+						ApplyOneClick(go);
+					}
+				}
+				else
+				{
+					ApplyOneClick(go);
+				}
+#else
+			ApplyOneClick(go);
+#endif
+		}
+
+		private static void ApplyOneClick(GameObject go)
+		{
+			SalsaOneClickSetup(go, AssetDatabase.LoadAssetAtPath<AudioClip>(OneClickBase.RESOURCE_CLIP));
+			EyesOneClickSetup(Selection.activeGameObject);
+		}
+	}
+}
---
+++ Assets/Plugins/Crazy Minnow Studio/SALSA LipSync/Plugins/OneClickRuntimes/OneClickReadyPlayerMeEyes.cs
@@ -0,0 +1,88 @@
+﻿using UnityEngine;
+
+namespace CrazyMinnow.SALSA.OneClicks
+{
+	public class OneClickReadyPlayerMeEyes : MonoBehaviour
+	{
+		public static void Setup(GameObject go)
+		{
+			string head = "^head$";
+			string[] body = new string[] {".+(head|avatar)$"};
+			string[] eyeL = new string[] {"LeftEye$"};
+			string[] eyeR = new string[] {"RightEye$"};
+			string[] blinkL = new string[] {"eyeBlinkLeft"};
+			string[] blinkR = new string[] {"eyeBlinkRight"};
+
+			if (go)
+			{
+				Eyes eyes = go.GetComponent<Eyes>();
+				if (eyes == null)
+				{
+					eyes = go.AddComponent<Eyes>();
+				}
+				else
+				{
+					DestroyImmediate(eyes);
+					eyes = go.AddComponent<Eyes>();
+				}
+				QueueProcessor qp = go.GetComponent<QueueProcessor>();
+				if (qp == null) qp = go.AddComponent<QueueProcessor>();
+
+				// System Properties
+                eyes.characterRoot = go.transform;
+                eyes.queueProcessor = qp;
+
+                // Heads - Bone_Rotation
+                eyes.BuildHeadTemplate(Eyes.HeadTemplates.Bone_Rotation_XY);
+                eyes.heads[0].expData.name = "head";
+                eyes.heads[0].expData.components[0].name = "head";
+                eyes.heads[0].expData.controllerVars[0].bone = Eyes.FindTransform(eyes.characterRoot, head);
+                eyes.headTargetOffset.y = 0.065f;
+                eyes.CaptureMin(ref eyes.heads);
+                eyes.CaptureMax(ref eyes.heads);
+
+                // Eyes - Bone_Rotation
+                eyes.BuildEyeTemplate(Eyes.EyeTemplates.Bone_Rotation);
+                eyes.eyes[0].expData.controllerVars[0].bone = Eyes.FindTransform(eyes.characterRoot, eyeL);
+                eyes.eyes[0].expData.name = "eyeL";
+                eyes.eyes[0].expData.components[0].name = "eyeL";
+                eyes.eyes[1].expData.controllerVars[0].bone = Eyes.FindTransform(eyes.characterRoot, eyeR);
+                eyes.eyes[1].expData.name = "eyeR";
+                eyes.eyes[1].expData.components[0].name = "eyeR";
+                eyes.CaptureMin(ref eyes.eyes);
+                eyes.CaptureMax(ref eyes.eyes);
+
+                // Eyelids - Bone_Rotation
+                eyes.BuildEyelidTemplate(Eyes.EyelidTemplates.BlendShapes, Eyes.EyelidSelection.Upper); // includes left/right eyelid
+                float blinkMax = 0.01f;
+                // Left eyelid
+                eyes.blinklids[0].expData.controllerVars[0].smr = Eyes.FindTransform(eyes.characterRoot,  body).GetComponent<SkinnedMeshRenderer>();
+                eyes.blinklids[0].expData.controllerVars[0].blendIndex = Eyes.FindBlendIndex(eyes.blinklids[0].expData.controllerVars[0].smr, blinkL);
+                eyes.blinklids[0].expData.controllerVars[0].maxShape = blinkMax;
+                eyes.blinklids[0].expData.name = "eyelidL";
+                // Right eyelid
+                eyes.blinklids[1].expData.controllerVars[0].smr = eyes.blinklids[0].expData.controllerVars[0].smr;
+                eyes.blinklids[1].expData.controllerVars[0].blendIndex = Eyes.FindBlendIndex(eyes.blinklids[0].expData.controllerVars[0].smr, blinkR);
+                eyes.blinklids[1].expData.controllerVars[0].maxShape = blinkMax;
+                eyes.blinklids[1].expData.name = "eyelidR";
+
+                // Track lids
+                eyes.CopyBlinkToTrack();
+                if (eyes.tracklids.Count > 1)
+                {
+	                eyes.tracklids[0].referenceIdx = 0; // left eye
+	                eyes.tracklids[1].referenceIdx = 1; // right eye
+                }
+
+                if (eyes.tracklids.Count > 3)
+                {
+	                eyes.tracklids[2].referenceIdx = 0; // left eye
+	                eyes.tracklids[3].referenceIdx = 1; // right eye
+                }
+
+                // Initialize the Eyes module
+                eyes.Initialize();
+			}
+		}
+	}
+}
